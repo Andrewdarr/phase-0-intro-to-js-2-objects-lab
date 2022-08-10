86deb1ce2ca5260a3fa88690d7d1e27504d4c5fb
Andrew Darr <kildar07@gmail.com>
2022-08-10 10:59:39 -0500

    commit tree 241483dcc2fc2d64294692bc7b1ad9b2d2aa8458

--- index.js
+++ index.js
@@ -1,1 +1,86 @@
-// Write your solution in this file!
+const employee = {
+    name: 'name',
+    streetAddress: 'address',
+    
+}// describe('employees', function() {
+//     describe('updateEmployeeWithKeyAndValue(employee, key, value)', function () {
+//       beforeEach(function () {
+//         for (const key in employee) {
+//           delete employee[key];
+//         }
+//         employee.name = 'Sam';
+//       });
+
+// it('returns an employee with the original key value pairs and the new key value pair', function () {
+//     expect(updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway')).to.eql({
+//       name: 'Sam',
+//       streetAddress: '11 Broadway'
+//     });
+//   });
+
+// it('it does not modify the original employee, but rather returns a clone with the new data', function () {
+//     updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway');
+//     expect(employee['streetAddress']).to.equal(undefined);
+//   });
+// });
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updateEmployee = {...employee };
+    updateEmployee[key] = value
+    return updateEmployee
+}
+
+// describe('destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)', function () {
+//     it('updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee', function () {
+//       expect(destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway')).to.eql({
+//         name: 'Sam',
+//         streetAddress: '12 Broadway'
+//       });
+//       expect(employee).to.eql({
+//         name: 'Sam',
+//         streetAddress: '12 Broadway'
+//       });
+//     });
+//   });
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    const updateEmployee = employee;
+    updateEmployee[key] = value;
+    return updateEmployee
+}
+
+// describe('deleteFromEmployeeByKey(employee, key)', function () {
+//     it('deletes `key` from a clone of employee and returns the new employee (it is non-destructive)', function () {
+//       let newEmployee = deleteFromEmployeeByKey(employee, 'name');
+//       expect(newEmployee['name']).to.equal(undefined);
+//       expect(typeof newEmployee).to.equal('object');
+//     });
+//     it('does not modify the original employee (it is non-destructive)', function () {
+//       deleteFromEmployeeByKey(employee, 'name');
+//       expect(employee['name']).to.equal('Sam');
+//     });
+//   });
+function deleteFromEmployeeByKey(employee, key, value) {
+    const updateEmployee = {...employee}
+    delete updateEmployee[key];
+    return updateEmployee
+}
+
+// describe('destructivelyDeleteFromEmployeeByKey(employee, key)', function () {
+//     it('returns employee without the deleted key/value pair', function () {
+//       let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+
+//       expect(newEmployee['name']).to.equal(undefined);
+//     });
+//     it('modifies the original employee', function () {
+//       let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+//       expect(employee['name']).to.equal(undefined);
+//       expect(employee).to.equal(newEmployee);
+//     });
+//   });
+// });
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    const updateEmployee = employee
+    delete updateEmployee[key]
+    return updateEmployee
+}
